81f38453c2c0f3ed54aadc900bd837e2ab44c37a
Cuong Ninh <ninhqcuong2002@gmail.com>
2024-07-15 09:34:50 +0700

    Update get location by address

--- app/src/main/java/com/example/loving_essentials/Domain/Services/API/APIClient.java
+++ app/src/main/java/com/example/loving_essentials/Domain/Services/API/APIClient.java
@@ -14,7 +14,7 @@ public class APIClient {
     //private static final String BASE_URL = "https://3831-27-74-201-205.ngrok-free.app/";
 
 
-    private static final String BASE_URL = " https://6a35-14-186-69-93.ngrok-free.app/api/";
+    private static final String BASE_URL = "https://7a41-118-69-70-166.ngrok-free.app/api/";
     private static final String NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/";
     private static final String OSRM_BASE_URL = "http://router.project-osrm.org/";
 
